452e6b15dc7eed6782cb759b8a116405ea9003d1
yuliusjati <yulius_jati_28rpl@student.smktelkom-mlg.sch.id>
2021-06-06 16:24:49 -0700

    Tugas 4

---
+++ src/components/First.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: white;
+        padding: 10px 50px;
+        background-color: #bbb;
+    }
+</style>
+
+<script>
+export default {
+    name: "First",
+    props: {
+        msg : String
+    }
+}
+</script>
